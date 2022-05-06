b6ff53cf10c61faaefc19dc862475e208ffb619f
shadra10 <shadow999xp@hotmail.com>
2022-05-06 19:30:12 -0400

    Things actually work praise jebus

---
+++ src/Tippy/Simon/Data/Messages.cs
@@ -0,0 +1,13 @@
+using Dalamud;
+
+#pragma warning disable CS1591
+namespace Tippy
+{
+    public static class Messages
+    {
+        public static readonly Message[] IntroMessages =
+        {
+            new("INT-000001", Localization.Localize("INT-000001", "Hi, I'm Shen! I'm your new friend and assistant. I will help you get better at FFXIV!")),
+        };
+    }
+}
